b8e85cd610c1f02e71ec7e1dda219dea905afbe2
Sophiaborgeess <sophiaaborgeess@gmail.com>
2026-05-06 14:45:27 -0300

    06/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,11 +1,16 @@
+import "./App.css";
+
 export default function App()
 {
   return (
     <div>
-      <h1>Aula 02 - Estudo de componentes e Props
+      <h1>Aula 02 - Estudo de componentes e Props</h1>
 
-        
-      </h1>
+<div className="card">
+  <h3>Chamada para o Exemplo1</h3>
+
+</div>
+      
     </div>
   )
 }
